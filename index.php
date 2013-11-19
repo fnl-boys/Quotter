9340eab6027bc6e01407a400edaafa531a5544e5
JewelsonF <j2e7w6e5l0s3o3n2@gmail.com>
2013-11-19 23:30:36 +0530

    initial structure

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width"/>
+		<title>Quotter</title>
+		<link rel="stylesheet" href="css/foundation.min.css" />
+		<link rel="stylesheet" href="css/style.css" />
+		<script src="js/custom.modernizr.js"></script>
+	</head>
+	<body>
+		<!--header starts-->
+		<div class="header">
+			<nav class="top-bar row">
+			  <ul class="title-area">
+				<!-- Title Area -->
+				<li class="name">
+				  <h1><a href="#">Top Bar Title </a></h1>
+				</li>
+				<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+				<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+			  </ul>
+
+			  <section class="top-bar-section">
+				<!-- Left Nav Section -->
+				<ul class="left">
+				  <li class="divider"></li>
+				  <li class="active"><a href="#">Main Item 1</a></li>
+				  <li class="divider"></li>
+				  <li><a href="#">Main Item 2</a></li>
+				  <li class="divider"></li>
+				</ul>
+
+				<!-- Right Nav Section -->
+				<ul class="right">
+				  <li class="divider hide-for-small"></li>
+				  <li class="has-dropdown"><a href="#">Main Item 4</a></li>
+				  <li class="divider"></li>
+				  <li class="has-form">
+					<form>
+					  <div class="row collapse">
+						<div class="small-8 columns">
+						  <input type="text">
+						</div>
+						<div class="small-4 columns">
+						  <a href="#" class="alert button">Search</a>
+						</div>
+					  </div>
+					</form>
+				  </li>
+				  <li class="divider show-for-small"></li>
+				  <li class="has-form">
+					<a class="button" href="#">Button!</a>
+				  </li>
+				</ul>
+			  </section>
+			</nav>	
+		</div>
+		<!--header ends-->
+		
+		<script src="js/jquery.js"></script>
+		<script src="js/foundation.min.js"></script>
+		<script>
+			$(document).foundation();
+		</script>
+	</body>
+	
+</html>
